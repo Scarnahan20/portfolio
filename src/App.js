55dfb0159dd0e930513b0b327a5b0455069c4ce5
Stefan Carnahan <stefancarnahan1@gmail.com>
2021-05-16 14:40:39 -0700

    Framework implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Projects from './components/Projects.js';
+import About from './components/About.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        <Route exact path="/" component={About} />
+        <Route path="/projects" component={Projects} />
+
+        <div className="navigation">
+          <div className="navigation-sub">
+
+            <Link to="/about" className="item">About</Link>
+            <Link to="/" className="item">Projects</Link>
+
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Thumbnail from "./Thumbnail"
+import '../App.css';
+
+function Projects(props) {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <Thumbnail link="https://github.com/Scarnahan20/fightboi_app" image="" title="" category="" />
+    </div>
+  )
+}
+
+export default Projects;
