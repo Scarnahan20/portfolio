5b4a171ef4b0f694abe7e902e5c0ed32d2d457ea
Stefan Carnahan <stefancarnahan1@gmail.com>
2021-05-16 14:58:35 -0700

    Implemented bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Projects from './components/Projects.js';
 import About from './components/About.js';
 import './App.css';
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
 
+        <Container fluid>
+          <Navbar bg="light" expand='lg'>
+            <Navbar.Brand>Stefan Carnahan</Navbar.Brand>
+            <Nav.Link href="/">About</Nav.Link>
+            <Nav.Link href="/projects">Projects</Nav.Link>
+          </Navbar>
+        </Container>
+
+
         <Route exact path="/" component={About} />
         <Route path="/projects" component={Projects} />
 
-        <div className="navigation">
-          <div className="navigation-sub">
-
-            <Link to="/about" className="item">About</Link>
-            <Link to="/" className="item">Projects</Link>
-
-          </div>
-        </div>
       </div>
     </BrowserRouter>
   );
